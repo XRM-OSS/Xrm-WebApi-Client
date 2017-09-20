8438d024c9d90baaf04bc4506a03bd63e5afa390
Florian Krönert <Florian.Kroenert@silflo.de>
2017-09-21 00:32:42 +0200

    Extended JSDoc

--- src/js/WebApiClient.BatchRequest.js
+++ src/js/WebApiClient.BatchRequest.js
@@ -1,16 +1,57 @@
 (function(undefined) {
     "use strict";
 
+    /**
+     * Request used inside batches, used for all HTTP methods
+     * @constructor
+     * @see https://msdn.microsoft.com/en-us/library/mt607719.aspx#bkmk_BatchRequests
+     * @param {Object} parameters
+     * @param {String} parameters.method The HTTP method such as GET, POST, ... for this request
+     * @param {String} parameters.url The url used for this request
+     * @param {Object} [parameters.payload] The request body for this request. Will be stringified and embedded.
+     * @param {Array<{key:string,value:string}>} [parameters.headers] Headers to append to this request
+     * @param {String} parameters.contentId Content ID to set for this request
+     * @memberof module:WebApiClient
+     */
     var BatchRequest = function (parameters) {
         var params = parameters || {};
 
+        /**
+         * @property {String} method - Method of the request such as GET, POST, ...
+         * @this {BatchRequest}
+         */
         this.method = params.method;
+
+        /**
+         * @property {String} url - URL for this request
+         * @this {BatchRequest}
+         */
         this.url = params.url;
+
+        /**
+         * @property {Object} payload - Payload to send with this request
+         * @this {BatchRequest}
+         */
         this.payload = params.payload;
+
+        /**
+         * @property {Array<{key: string, value:string}>} headers - Headers to append to this request
+         * @this {BatchRequest}
+         */
         this.headers = params.headers || [];
+
+        /**
+         * @property {String} contentId - Content ID for this request. Will be set on the responses as well, to match responses with requests
+         * @this {BatchRequest}
+         */
         this.contentId = params.contentId;
     };
 
+    /**
+     * @description Converts current batch request into a string representation for including in the batch body
+     * @return {String}
+     * @this {BatchRequest}
+     */
     BatchRequest.prototype.stringify = function () {
         var payload = "";
 
--- src/js/WebApiClient.ChangeSet.js
+++ src/js/WebApiClient.ChangeSet.js
@@ -3,13 +3,38 @@
 
     var instanceCount = 1;
 
+    /**
+     * Change sets are containers for requests inside batch requests.
+     * All requests inside a change set fail or succeed together.
+     * No GET requests are allowed inside change sets.
+     * @constructor
+     * @see https://msdn.microsoft.com/en-us/library/mt607719.aspx#bkmk_ChangeSets
+     * @param {Object} [parameters]
+     * @param {String} [parameters.name] The name of the change set (should be unique for this batch). Auto generated if ommitted
+     * @param {Array<Request>} [parameters.requests] Array of _POST_ requests for this change set. No get requests are allowed inside change sets. Initialized as empty array if ommitted
+     * @memberof module:WebApiClient
+     */
     var ChangeSet = function (parameters) {
         var params = parameters || {};
 
+        /**
+         * @property {String} name - Name of the change set
+         * @this {ChangeSet}
+         */
         this.name = params.name || "changeset_" + instanceCount++;
+
+        /**
+         * @property {Array<Request>} requests - Requests included in the change set. Only non GET requests are allowed.
+         * @this {ChangeSet}
+         */
         this.requests = params.requests || [];
     };
 
+    /**
+     * @description Converts current change set into a string representation for including in the batch body
+     * @return {String}
+     * @this {ChangeSet}
+     */
     ChangeSet.prototype.stringify = function () {
         var payload = "";
         var contentId = 1;
